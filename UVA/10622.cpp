00d1f52b468bd87a15ab8c888d10d0b17749ac80
Ayman Salah <ayman.salah1996@gmail.com>
2017-06-27 14:21:45 +0200

    Create 10622.cpp

---
+++ UVA/10622.cpp
@@ -0,0 +1,95 @@
+	#define _CRT_SECURE_NO_WARNINGS
+	#include <cstring>
+	#include <map>
+	#include <deque>
+	#include <queue>
+	#include <stack>
+	#include <sstream>
+	#include <numeric>
+	#include <iostream>
+	#include <iomanip>
+	#include <cstdio>
+	#include <cmath>
+	#include <hash_map>
+	#include <fstream>
+	#include <string>
+	#include <cstdlib>
+	#include <ctime>
+	#include <functional>
+	#include <algorithm>
+	#include <vector>
+	#include <set>
+	#include <complex>
+	#include <list>
+	#include <climits>
+	#include <cctype>
+	#include <bitset>
+	using namespace std;
+
+	#define PI 3.14159265359
+	#define all(v) v.begin(),v.end()
+	#define sortva(v) sort(all(v))
+	#define sortvd(v) sort(v.rbegin(),v.rend())
+	#define sortaa(a,n) sort(a,a+n)
+	#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+	#define sfi1(v) scanf("%d",&v)
+	#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+	#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+	#define sfll1(v) scanf("%I64d",&v);
+	#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+	#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+	#define sfstr(v) scanf("%s", v);
+	#define sz(v) (int)v.size()
+	#define ndl puts("")
+	#define SS stringstream
+	typedef long long ll;
+	typedef unsigned long long ull;
+	int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+	int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+	ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+	ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+	void PLAY() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+		cout << fixed << setprecision(1);
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+	}
+
+
+	int main() {
+		PLAY();
+
+		map<ll, ll> mp;
+		for (ll i = 2; i <= 46341; i++) {
+			ll tmp = i, cnt = 1;
+			while (tmp <= INT_MAX) {
+				mp[tmp] = max(cnt, mp[tmp]);
+				tmp *= i;
+				cnt++;
+			}
+		}
+
+		for (ll i = -2; i >= -46341; i--) {
+			ll tmp = i, cnt = 1;
+			while (tmp >= INT_MIN) {
+				mp[tmp] = max(cnt, mp[tmp]);
+				tmp *= i; tmp *= i;
+				cnt += 2;
+			}
+		}
+		ll n;
+		while (cin >> n && n) {
+			if (mp.find(n) == mp.end())
+				cout << 1 << endl;
+			else
+				cout << mp[n] << endl;
+		}
+	
+		return 0;
+	}
