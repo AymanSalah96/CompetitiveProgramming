a2ad024d0c765efbd010b42935a091a5acaeb52f
AymanSalah96 <ayman.salah1996@gmail.com>
2017-11-16 18:08:21 +0200

    add CF358-D2-D

---
+++ Codeforces/CF358-D2-D.cpp
@@ -0,0 +1,96 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << fixed << setprecision(10);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+
+int n;
+int a[3005], b[3005], c[3005];
+int dp[3005][2];
+
+int solve(int i, int t) {
+	if (i == n - 1) {
+		if (t) return b[i];
+		return a[i];
+	}
+	int &ret = dp[i][t];
+	if (ret != -1) return ret;
+	if (t) ret = max(b[i] + solve(i + 1, 1), c[i] + solve(i + 1, 0));
+	else ret = max(a[i] + solve(i + 1, 1), b[i] + solve(i + 1, 0));
+	return ret;
+}
+
+int main() {
+	PLAY();
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	for (int i = 0; i < n; i++)
+		cin >> b[i];
+	for (int i = 0; i < n; i++)
+		cin >> c[i];
+
+	memset(dp, -1, sizeof dp);
+	cout << solve(0, 0) << endl;
+
+	return 0;
+}
