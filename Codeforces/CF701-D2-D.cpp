3d395a84a9b25a31f682cf3af4d44eb08f5f8cb8
Ayman Salah <ayman.salah1996@gmail.com>
2017-11-18 16:19:22 +0200

    Create CF701-D2-D.cpp

---
+++ Codeforces/CF701-D2-D.cpp
@@ -0,0 +1,83 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << fixed << setprecision(10);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int main() {
+	PLAY();
+
+	int n, l, v1, v2, k;
+	cin >> n >> l >> v1 >> v2 >> k;
+
+	double lo = 0, hi = 1e18;
+	for (int x = 0; x < 500; x++) {
+		double mid = (lo + hi) / 2.0;
+		double dist = l - mid * v1;
+		double t1 = dist / (v2 - v1), t2 = dist / (v2 + v1), t = 0;
+		for (int i = 0; i < n; i += k)
+			t += (i + k < n) ? t1 + t2 : t1;
+		if (t < mid) hi = mid;
+		else lo = mid;
+	}
+	cout << (lo + hi) / 2 << endl;
+
+	return 0;
+}
