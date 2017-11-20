7e22b1ee2cf1a5243b978746995d5c27a19c75e1
Ayman Salah <ayman.salah1996@gmail.com>
2017-11-21 00:19:02 +0200

    Create CITY2.cpp

---
+++ SPOJ/CITY2.cpp
@@ -0,0 +1,116 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << fixed << setprecision(10);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int v[100005];
+int tree[1100000];
+
+void build(int cur, int l, int r) {
+	if (l == r) {
+		tree[cur] = v[l];
+		return;
+	}
+	int left = cur * 2;
+	int right = cur * 2 + 1;
+	int mid = (l + r) / 2;
+	build(left, l, mid);
+	build(right, mid + 1, r);
+	tree[cur] = min(tree[left], tree[right]);
+}
+
+int solve(int cur, int l, int r, int x, int y) {
+	if (l > y || r < x) return INT_MAX;
+	if (l >= x && r <= y) return tree[cur];
+	int left = cur * 2;
+	int right = cur * 2 + 1;
+	int mid = (l + r) / 2;
+	return min(solve(left, l, mid, x, y), solve(right, mid + 1, r, x, y));
+}
+
+int main() {
+	PLAY();
+
+
+	int n;
+	int test = 1;
+	while (cin >> n) {
+		cout << "Case " << test++ << ": ";
+		for (int i = 1; i <= n; i++)
+			cin >> v[i], tree[i] = INT_MAX;
+		build(1, 1, n);
+		map<int, int> lastSeen;
+		int res = 0;
+		for (int i = 1; i <= n; i++) {
+			if (v[i] == 0) continue;
+			if (!lastSeen[v[i]]) lastSeen[v[i]] = i, res++;
+			else {
+				int idx = lastSeen[v[i]];
+				lastSeen[v[i]] = i;
+				int mn = solve(1, 1, n, idx, i);
+				if (mn < v[i]) res++;
+			}
+		}
+		cout << res << endl;
+	}
+
+	return 0;
+}
