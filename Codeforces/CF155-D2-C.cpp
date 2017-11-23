306ed118e7a7e9c4e759193a1a4e2a40561e44ab
Ayman Salah <ayman.salah1996@gmail.com>
2017-11-23 13:51:12 +0200

    Create CF155-D2-C.cpp

---
+++ Codeforces/CF155-D2-C.cpp
@@ -0,0 +1,98 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << fixed << setprecision(10);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+string s;
+bool forb[50][50];
+
+int dp[100005][50];
+
+int solve(int idx, int prev) {
+	if (idx == sz(s)) return 0;
+	int &ret = dp[idx][prev];
+	if (ret != -1) return ret;
+	int cur = s[idx] - 'a';
+	ret = solve(idx + 1, prev) + 1;
+
+	if (!forb[prev][cur])
+		ret = min(ret, solve(idx + 1, cur));
+	
+	return ret;
+}
+
+int main() {
+	PLAY();
+
+	cin >> s;
+	int m; cin >> m;
+	while (m--) {
+		string x;
+		cin >> x;
+		forb[x[0] - 'a'][x[1] - 'a'] = true;
+		forb[x[1] - 'a'][x[0] - 'a'] = true;
+	}
+
+	memset(dp, -1, sizeof dp);
+	cout << solve(0, 27) << endl;
+
+	return 0;
+}
