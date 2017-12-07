bbb6cdbff188ef12a3883d465284650d8dd7b747
Ayman Salah <ayman.salah1996@gmail.com>
2017-12-07 11:09:34 +0200

    Create CF255-D2-D.cpp

---
+++ Codeforces/CF255-D2-D.cpp
@@ -0,0 +1,100 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define flush fflush(stdout)
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	cout << fixed << setprecision(10);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+ll n, x, y, c;
+
+bool valid(ll t) {
+	ll left = max(1LL, y - t);
+	ll right = min(n, y + t);
+	ll cnt = 0;
+	for (ll i = left; i <= right; i++) {
+		ll tmp = t - abs(y - i);
+		ll up = max(1LL, x - tmp);
+		ll down = min(n, x + tmp);
+		cnt += down - up + 1;
+		if (cnt >= c) return true;
+	}
+	return false;
+}
+
+int main() {
+	PLAY();
+
+	cin >> n >> x >> y >> c;
+	if (c == 1) cout << 0 << endl;
+	else {
+		ll lo = 1, hi = 1e18, res = 0;
+		while (lo <= hi) {
+			ll mid = (lo + hi) / 2;
+			if (valid(mid)) {
+				res = mid;
+				hi = mid - 1;
+			}
+			else lo = mid + 1;
+		}
+		cout << res << endl;
+	}
+	return 0;
+}
