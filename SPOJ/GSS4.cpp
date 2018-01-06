b734089f8eb230f4532510ac5df91f43ab7f3f98
Ayman Salah <ayman.salah1996@gmail.com>
2018-01-06 22:36:09 +0200

    Create GSS4.cpp

---
+++ SPOJ/GSS4.cpp
@@ -0,0 +1,161 @@
+/*
+Author: Ayman Salah
+
+Category: segment tree
+
+Idea:
+- First neglext any non positive number in the given array
+- the worst case for updating that we have all the numbers 10^18 so you need 6 steps only to reach to 1
+- and you don't need to make update query on that number again
+- we will have a set of indices that have value greater than 1 and after each update if the value of that index equal 1 remove it
+- use segment tree to update each index and answer the range sum query
+*/
+
+
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << fixed << setprecision(10);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+ll tree[1100000];
+ll a[100005];
+
+void build(int cur, int l, int r) {
+	if (l == r) {
+		tree[cur] = a[l];
+		return;
+	}
+	int left = cur * 2;
+	int right = cur * 2 + 1;
+	int mid = (l + r) / 2;
+	build(left, l, mid);
+	build(right, mid + 1, r);
+	tree[cur] = tree[left] + tree[right];
+}
+
+
+ll query(int cur, int l, int r, int x, int y) {
+	if (l > y || r < x) return 0;
+	if (l >= x && r <= y) return tree[cur];
+	int left = cur * 2;
+	int right = cur * 2 + 1;
+	int mid = (l + r) / 2;
+	return query(left, l, mid, x, y) + query(right, mid + 1, r, x, y);
+}
+
+void update(int cur, int l, int r, int idx) {
+	if (l == r && l == idx) {
+		tree[cur] = a[idx];
+		return;
+	}
+	int left = cur * 2;
+	int right = cur * 2 + 1;
+	int mid = (l + r) / 2;
+	if (idx <= mid)
+		update(left, l, mid, idx);
+	else
+		update(right, mid + 1, r, idx);
+	tree[cur] = tree[left] + tree[right];
+}
+
+int main() {
+	PLAY();
+	
+	set<int>::iterator it;
+	int n;
+	int test = 1;
+
+	while (cin >> n) {
+		cout << "Case #" << test++ << ":\n";
+		set<int> ava;
+		for (int i = 1; i <= n; i++) {
+			cin >> a[i];
+			if (a[i] > 0)
+				ava.insert(i);
+		}
+		build(1, 1, n);
+		int q;
+		cin >> q;
+		while (q--) {
+			int type, l, r;
+			cin >> type >> l >> r;
+			if (l > r) swap(l, r);
+			if (type == 0) {
+				it = ava.lower_bound(l);
+				if (it == ava.end()) continue;
+				int idx = *it;
+				while (idx <= r) {
+					a[idx] = (ll)sqrt(a[idx]);
+					if (a[idx] <= 1) ava.erase(it);
+					update(1, 1, n, idx);
+					it = ava.upper_bound(idx);
+					if (it == ava.end()) break;
+					idx = *it;
+				}
+			}
+			else
+				cout << query(1, 1, n, l, r) << "\n";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
