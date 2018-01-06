bad3246f7d48c2647c29b4127db99f053e5c2906
Ayman Salah <ayman.salah1996@gmail.com>
2018-01-06 15:36:27 +0200

    Create CF476-D2-D.cpp

---
+++ Codeforces/CF476-D2-D.cpp
@@ -0,0 +1,85 @@
+/*
+Author: Ayman Salah
+
+Category: Number theory, Constructive Algorithms
+
+Idea:
+- If we get a set of rank 1 and multiply it by k then we will get the answer
+- We can get a set of rank 1 by this {(6*i + 1), (6*i + 2), (6*i + 3), (6*i + 5)}
+- k will equal (6*n - 1)*k
+*/
+
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << fixed << setprecision(10);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int main() {
+	PLAY();
+
+	int n, k;
+	cin >> n >> k;
+	cout << (6*n - 1) * k << endl;
+	for (int i = 0; i < n; i++)
+		cout << (6 * i + 1)* k << " " << (6 * i + 2)*k << " " << (6 * i + 3)*k << " " << (6 * i + 5)*k << "\n";
+
+	return 0;
+}
