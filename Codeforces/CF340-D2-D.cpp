661bbde673d37403ac02a06c78ec214483ec2568
Ayman Salah <ayman.salah1996@gmail.com>
2018-02-14 15:43:50 +0200

    Create CF340-D2-D.cpp

---
+++ Codeforces/CF340-D2-D.cpp
@@ -0,0 +1,101 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << fixed << setprecision(10);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int n;
+int arr[100005];
+
+int idx(vector<int> &a, int l, int r, int val) {
+	while (r - l > 1) {
+		int mid = l + (r - l) / 2;
+		if (a[mid] >= val) r = mid;
+		else l = mid;
+	}
+	return r;
+}
+
+int LIS() {
+	vector<int> v(n, 0);
+	int ret = 1;
+	v[0] = arr[0];
+	for (size_t i = 1; i < n; i++) {
+		if (arr[i] < v[0])
+			v[0] = arr[i];
+		else if (arr[i] > v[ret - 1])
+			v[ret++] = arr[i];
+		else
+			v[idx(v, -1, ret - 1, arr[i])] = arr[i];
+	}
+
+	return ret;
+}
+
+int main() {
+	PLAY();
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	cout << LIS() << endl;
+
+	return 0;
+}
