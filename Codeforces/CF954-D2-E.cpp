509ddf20c2b15a66fb5b70d3a0f64549c11d7d57
Ayman Salah <ayman.salah1996@gmail.com>
2018-03-23 02:42:30 +0200

    Create CF954-D2-E.cpp

---
+++ Codeforces/CF954-D2-E.cpp
@@ -0,0 +1,116 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(first,n) sort(first,first+n)
+#define sortad(first,n) sort(first,first+n),reverse(first,first+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%sum", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define flush fflush(stdout)
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll first, ll second) { return !second ? first : gcd(second, first % second); }
+ll lcm(ll first, ll second) { return (first / gcd(first, second)) * second; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	cout << fixed << setprecision(15);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+const int MAX = 5e5;
+pair<ld, ld> tabs[MAX];
+
+
+int main() {
+	PLAY();
+
+	int n;
+	ld temp;
+
+	cin >> n >> temp;
+
+	for (int i = 0; i < n; i++)
+		cin >> tabs[i].second;
+	
+	for (int i = 0; i < n; i++)
+		cin >> tabs[i].first;
+	
+	sortaa(tabs, n);
+
+
+	if (temp < tabs[0].first || temp > tabs[n - 1].first)
+		cout << 0 << endl;
+	else {
+		ld sum = 0;
+		for (int i = 0; i < n; i++) {
+			tabs[i].first -= temp;
+			sum += tabs[i].first * tabs[i].second;
+		}
+		if (sum < 0) {
+			sum *= -1;
+			for (int i = 0; i < n; i++)
+				tabs[i].first *= -1;
+			sortaa(tabs, n);
+		}
+		sum = 0;
+		ld res = 0;
+		for (int i = 0; i < n; i++) {
+			ld tmp = sum + tabs[i].first * tabs[i].second;
+			if (tmp > 0) {
+				res -= sum / tabs[i].first;
+				break;
+			}
+			res += tabs[i].second;
+			sum += tabs[i].first * tabs[i].second;
+		}
+		cout << res << endl;
+	}
+
+	return 0;
+}
