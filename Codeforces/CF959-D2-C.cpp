6799b98064516aede4eddcadf9cca629a03559ae
Ayman Salah <ayman.salah1996@gmail.com>
2018-04-03 21:13:16 +0200

    Create CF959-D2-C.cpp

---
+++ Codeforces/CF959-D2-C.cpp
@@ -0,0 +1,92 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <assert.h>
+using namespace std;
+
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(first,n) sort(first,first+n)
+#define sortad(first,n) sort(first,first+n),reverse(first,first+n)
+#define sfi1(v) scanf("%dist",&v)
+#define sfi2(v1,v2) scanf("%dist %dist",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%dist %dist %dist",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%sum", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define flush fflush(stdout)
+#define SS stringstream
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+ll gcd(ll first, ll second) { return !second ? first : gcd(second, first % second); }
+ll lcm(ll first, ll second) { return (first / gcd(first, second)) * second; }
+
+void PLAY() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	cout << fixed << setprecision(5);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int main() {
+	PLAY();
+	
+	int n;
+	cin >> n;
+
+	int rem = n - 2, nxt = 3;
+	int first = rem / 2;
+	int second = rem - first;
+
+
+	if (min(1 + second, 1 + first) <= 2) {
+		cout << -1 << endl;
+	}
+	else {
+		cout << "1 2" << "\n";
+		while (first--)
+			cout << 1 << " " << nxt++ << "\n";
+		while (second--)
+			cout << 2 << " " << nxt++ << "\n";
+	}
+	for (int i = 2; i <= n; i++)
+		cout << 1 << " " << i << "\n";
+
+	return 0;
+}
