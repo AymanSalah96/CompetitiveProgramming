ac4e26dcf167ee8e4c6bdf5816825cb88b5ee04f
Ayman Salah <ayman.salah1996@gmail.com>
2020-02-28 03:59:52 +0200

    Add InterviewBit Solutions

---
+++ InterviewBit/noble-integer.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solve(ArrayList<Integer> A) {
+        Collections.sort(A);
+        for(int i = 0; i < A.size(); i++) {
+			while(i + 1 < A.size() && A.get(i) == A.get(i + 1))
+				i++;
+            int rem = A.size() - i - 1;
+            if(rem == A.get(i))
+                return 1;
+        }
+        return -1;
+    }
+}
