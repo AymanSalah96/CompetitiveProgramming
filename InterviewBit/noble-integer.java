46fd5c26600ef78197fc80bf6d2ae2f1887dda88
Ayman Salah <ayman.salah1996@gmail.com>
2020-02-28 04:04:44 +0200

    Fix indent

--- InterviewBit/noble-integer.java
+++ InterviewBit/noble-integer.java
@@ -3,7 +3,7 @@ public class Solution {
         Collections.sort(A);
         for(int i = 0; i < A.size(); i++) {
             while(i + 1 < A.size() && A.get(i) == A.get(i + 1))
-				i++;
+                i++;
             int rem = A.size() - i - 1;
             if(rem == A.get(i))
                 return 1;
